46d6e24d6a332ef431b72730356655e01710f42e
Adon Metcalfe <adonm@fastmail.fm>
2021-10-16 08:23:40 +0800

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,7 +3,25 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "image1":
+            case "Apider":return img`
+. . . . . . . . . 2 . . . . . . 
+. . . . . . 2 2 . 2 . . . . . . 
+. . . . . . 2 2 . 2 . . . . . . 
+. . . 2 . . 2 . 2 2 . . . . . . 
+2 2 2 2 2 . 2 2 2 2 2 . . . 2 2 
+. . . 2 2 2 f 2 f f 2 2 2 2 2 . 
+. 2 2 . . f f f f f 2 2 . . . . 
+2 . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . 2 2 2 2 2 2 2 . . . . 
+. . . . 2 2 2 2 2 2 2 2 . . . . 
+. 2 2 2 2 2 2 2 2 2 . 2 2 . . . 
+. . . . . . . . . . . . 2 2 2 . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
 namespace SpriteKind {
     export const Plant = SpriteKind.create()
 }
+info.onCountdownEnd(function () {
+    effects.clearParticles(snake)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeScoreBy(-1)
+    otherSprite.startEffect(effects.trail)
+    sprite.sayText("Sorry")
+    info.startCountdown(2)
+    sprite.setVelocity(300, 300)
+    effects.clearParticles(sprite)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
+    Cakes += -1
     otherSprite.startEffect(effects.fire)
     scene.cameraShake(4, 500)
     otherSprite.destroy()
 })
+let myEnemy: Sprite = null
 let Fruit: Sprite = null
+let snake: Sprite = null
 scene.setBackgroundColor(2)
 tiles.setTilemap(tilemap`level2`)
-let snake = sprites.create(img`
+snake = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -30,28 +44,27 @@ let snake = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(snake)
 scene.cameraFollowSprite(snake)
-for (let index = 0; index < 11; index++) {
+game.splash("hi snake you like some cakes?")
+let Cakes = 9
+for (let index = 0; index < Cakes; index++) {
     tiles.setTileAt(tiles.getTileLocation(randint(0, 16), randint(0, 16)), img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . 6 . . . . . . . . . . 
-        . . . 6 . a a 6 . . . . . . . . 
-        . . . 6 . a a a . . . . a . . . 
-        . . . 6 a a a a . . . . a . . . 
-        . . . 6 a a . . . . . . a . . . 
-        . . . . a a . . . . a a 6 6 . . 
-        . . . a a a . . a a a a 6 6 . . 
-        . . . . . . . a a a a a 6 6 . . 
-        a a a . . . . a a a a 6 6 6 . . 
-        a . a a . . . a a a a a 6 6 . . 
-        a . a a . . . . . . . . . . . . 
-        a a a a a a . . . . . . . . . . 
-        . a a a a . . . . . . . . 6 6 . 
+        . . . 6 . 4 4 6 . . . . . . . . 
+        . . . 6 . 4 4 4 . . . . 4 . . . 
+        . . . 6 4 4 4 4 . . . . 4 . . . 
+        . . . 6 4 4 . . . . . . 4 . . . 
+        . . . . 4 4 . . . . 4 4 6 6 . . 
+        . . . 4 4 4 . . 4 4 4 4 6 6 . . 
+        . . . . . . . 4 4 4 4 4 6 6 . . 
+        4 4 4 . . . . 4 4 4 4 6 6 6 . . 
+        4 . 4 4 . . . 4 4 4 4 4 6 6 . . 
+        4 . 4 4 . . . . . . . . . . . . 
+        4 4 4 4 4 4 . . . . . . . . . . 
+        . 4 4 4 4 . . . . . . . . 6 6 . 
         . . . . . . . . . . . . . 6 . . 
         `)
-}
-game.splash("hi snake you like some cakes?")
-for (let index = 0; index < 9; index++) {
     Fruit = sprites.create(img`
         . . . . . . . . . . b b b . . . 
         . . . . . . . . b e e 3 3 b . . 
@@ -72,12 +85,61 @@ for (let index = 0; index < 9; index++) {
         `, SpriteKind.Food)
     Fruit.setPosition(randint(0, 256), randint(0, 256))
 }
-game.onUpdate(function () {
-    if (info.score() > 8) {
-        game.splash("dear mara I made this game.", "I hope you like my game")
-        game.over(true)
-    }
-})
+let Level = 0
 forever(function () {
     music.playMelody("E D G F B A C5 B ", 200)
+})
+game.onUpdateInterval(500, function () {
+    if (Cakes == 0 && Level == 0) {
+        Cakes = 10
+        effects.confetti.startScreenEffect()
+        scene.setBackgroundColor(10)
+        tiles.setTilemap(tilemap`level2`)
+        for (let index = 0; index < Cakes; index++) {
+            tiles.setTileAt(tiles.getTileLocation(randint(0, 16), randint(0, 16)), img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . 6 . . . . . . . . . . 
+                . . . 6 . c c 6 . . . . . . . . 
+                . . . 6 . c c c . . . . c . . . 
+                . . . 6 c c c c . . . . c . . . 
+                . . . 6 c c . . . . . . c . . . 
+                . . . . c c . . . . c c 6 6 . . 
+                . . . c c c . . c c c c 6 6 . . 
+                . . . . . . . c c c c c 6 6 . . 
+                c c c . . . . c c c c 6 6 6 . . 
+                c . c c . . . c c c c c 6 6 . . 
+                c . c c . . . . . . . . . . . . 
+                c c c c c c . . . . . . . . . . 
+                . c c c c . . . . . . . . 6 6 . 
+                . . . . . . . . . . . . . 6 . . 
+                `)
+            Fruit = sprites.create(img`
+                . . . . . . . . . . b b b . . . 
+                . . . . . . . . b e e 3 3 b . . 
+                . . . . . . b b e 3 2 e 3 a . . 
+                . . . . b b 3 3 e 2 2 e 3 3 a . 
+                . . b b 3 3 3 3 3 e e 3 3 3 a . 
+                b b 3 3 3 3 3 3 3 3 3 3 3 3 3 a 
+                b 3 3 3 d d d d 3 3 3 3 3 d d a 
+                b b b b b b b 3 d d d d d d 3 a 
+                b d 5 5 5 5 d b b b a a a a a a 
+                b 3 d d 5 5 5 5 5 5 5 d d d d a 
+                b 3 3 3 3 3 3 d 5 5 5 d d d d a 
+                b 3 d 5 5 5 3 3 3 3 3 3 b b b a 
+                b b b 3 d 5 5 5 5 5 5 5 d d b a 
+                . . . b b b 3 d 5 5 5 5 d d 3 a 
+                . . . . . . b b b b 3 d d d b a 
+                . . . . . . . . . . b b b a a . 
+                `, SpriteKind.Food)
+            Fruit.setPosition(randint(0, 256), randint(0, 256))
+        }
+        myEnemy = sprites.create(assets.image`Apider`, SpriteKind.Enemy)
+        myEnemy.follow(snake, 20)
+        Level = 1
+    } else if (Cakes == 0 && Level == 1) {
+        effects.confetti.startScreenEffect()
+        game.splash("dear mara I made this game.", "I hope you like my game")
+        game.over(true, effects.confetti)
+    }
 })
